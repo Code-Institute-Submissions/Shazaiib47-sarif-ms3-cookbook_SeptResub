3519eb1243469bff5b73a933eb2726708d59d736
Shazaiib47 <shazaib47@hotmail.com>
2021-05-07 15:54:45 +0000

    Added login functionality as well as changing the div to H3 element to contrast text to be much bigger on login/register template

--- app.py
+++ app.py
@@ -48,6 +48,32 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # This checks to see if the username already exists in the DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Validates that the hashed password matches what the user has typed.
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}.".format(request.form.get("username")))
+            else:
+                # triggers if password is matched incorrectly
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # triggers if username does not exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
