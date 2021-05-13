2424fef42c3e024d7aa20bcddc3e14f18bb1ffdc
Shazaiib47 <shazaib47@hotmail.com>
2021-05-13 05:23:19 +0000

    Added code that binds data from mongoDB to editing a recipe, as well as renaming variables around due to jinja errors. A cancel all changes button was also implemented

--- app.py
+++ app.py
@@ -119,11 +119,11 @@ def add_recipe():
     return render_template("add_recipe.html", recipes=recipes)
 
 
-@app.route("/edit_recipe/<recipes_id>", methods=["GET", "POST"])
-def edit_recipe(recipes_id):
-    recipes = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
-    recipes = mongo.db.recipe_categories.find().sort("recipe_category", 1)
-    return render_template("edit_recipe.html", recipes=recipes)
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    categories = mongo.db.recipe_categories.find().sort("recipe_category", 1)
+    return render_template("edit_recipe.html", recipe=recipe, categories=categories)
 
 
 if __name__ == "__main__":
